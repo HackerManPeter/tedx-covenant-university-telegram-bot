f49b884820a97c57297176eb669e5891f4d02077
Ebueku Uyiosa Peter <pebueku@gmail.com>
2022-04-01 23:16:50 +0100

    Added webhook

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 
+from flask import Flask, request
+
 import telebot
 from helper import get_start_markup, get_next_markup
 
@@ -12,6 +14,7 @@ PHOTO_ID = os.environ["PHOTO_ID"]
 CHANNEL = os.environ["CHANNEL"]
 
 bot = telebot.TeleBot(TOKEN)
+server = Flask(__name__)
 
 
 @bot.message_handler(commands=["start"], chat_types=["private"])
@@ -60,4 +63,20 @@ TEDxCovenantUniversity Community",
         )
 
 
-bot.infinity_polling()
+@server.route("/" + TOKEN, methods=["POST"])
+def getMessage():
+    json_string = request.get_data().decode("utf-8")
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url="https://tedx-cu-bot.herokuapp.com/" + TOKEN)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
