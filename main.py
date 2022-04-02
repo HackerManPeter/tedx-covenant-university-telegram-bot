7ce3d721e2e9baa02265fcd049c1373f46e05942
Ebueku Uyiosa Peter <pebueku@gmail.com>
2022-04-02 18:11:54 +0100

    Small changes

--- main.py
+++ main.py
@@ -90,16 +90,15 @@ def getMessage():
     json_string = request.get_data().decode("utf-8")
     update = telebot.types.Update.de_json(json_string)
     bot.process_new_updates([update])
-    return "?", 200
+    return "!", 200
 
 
 @server.route("/")
 def webhook():
     bot.remove_webhook()
     bot.set_webhook(url="https://tedx-cu-bot.herokuapp.com/" + TOKEN)
-    # bot.set_webhook(url="http://172.16.4.23:5000/" + TOKEN)
     return "!", 200
 
 
 if __name__ == "__main__":
-    server.run(deug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
