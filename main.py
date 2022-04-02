6e906551be15d0861f544c138f57e3dd25dee1a9
Ebueku Uyiosa Peter <pebueku@gmail.com>
2022-04-02 12:40:14 +0100

    Changed getUpdates() function

--- main.py
+++ main.py
@@ -87,7 +87,8 @@ TEDxCovenantUniversity Community",
 
 @server.route("/" + TOKEN, methods=["POST"])
 def getMessage():
-    json_string = request.get_data().decode("utf-8")
+    # json_string = request.get_data().decode("utf-8")
+    json_string = request.stream.read().decode("utf-8")
     update = telebot.types.Update.de_json(json_string)
     bot.process_new_updates([update])
     return "!", 200
