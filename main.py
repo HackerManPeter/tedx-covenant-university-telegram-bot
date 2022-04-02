c0d5d94d929fe5ebc18c4f050b4bc4c372f1c13c
Ebueku Uyiosa Peter <pebueku@gmail.com>
2022-04-02 14:44:11 +0100

    Dev commits

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 
-from flask import Flask, request
 
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
@@ -15,7 +14,6 @@ PHOTO_ID = os.environ["PHOTO_ID"]
 CHANNEL = os.environ["CHANNEL"]
 
 bot = telebot.TeleBot(TOKEN)
-server = Flask(__name__)
 
 
 # Heleper functions
@@ -85,21 +83,4 @@ TEDxCovenantUniversity Community",
         )
 
 
-@server.route("/" + TOKEN, methods=["POST"])
-def getMessage():
-    json_string = request.get_data().decode("utf-8")
-    update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return "?", 200
-
-
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url="https://tedx-cu-bot.herokuapp.com/" + TOKEN)
-    # bot.set_webhook(url="http://172.16.4.23:5000/" + TOKEN)
-    return "!", 200
-
-
-if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+bot.infinity_polling()
