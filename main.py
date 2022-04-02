5b4ca519e5499bcd134b4c9876fff8713f1994dc
Ebueku Uyiosa Peter <pebueku@gmail.com>
2022-04-02 13:06:08 +0100

    Changed from webhook to polling

--- main.py
+++ main.py
@@ -84,21 +84,21 @@ TEDxCovenantUniversity Community",
             reply_markup=get_next_markup(),
         )
 
-
-@server.route("/" + TOKEN, methods=["POST"])
-def getMessage():
-    json_string = request.get_data().decode("utf-8")
-    update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return "!", 200
-
-
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url="https://tedx-cu-bot.herokuapp.com/" + TOKEN)
-    return "!", 200
+bot.infinity_polling()
+# @server.route("/" + TOKEN, methods=["POST"])
+# def getMessage():
+#     json_string = request.get_data().decode("utf-8")
+#     update = telebot.types.Update.de_json(json_string)
+#     bot.process_new_updates([update])
+#     return "!", 200
 
 
-if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+# @server.route("/")
+# def webhook():
+#     bot.remove_webhook()
+#     bot.set_webhook(url="https://tedx-cu-bot.herokuapp.com/" + TOKEN)
+#     return "!", 200
+
+
+# if __name__ == "__main__":
+#     server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
