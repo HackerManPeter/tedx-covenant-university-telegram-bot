42daeeea54122bc8cd39561dfd13609cf93884e7
Ebueku Uyiosa Peter <pebueku@gmail.com>
2022-04-08 12:02:17 +0100

    revert

--- app/main.py
+++ app/main.py
@@ -4,7 +4,8 @@ from flask import Flask, request
 
 
 import telebot
-from helper import get_next_markup, get_start_markup
+from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -15,6 +16,27 @@ CHANNEL = os.environ["CHANNEL"]
 
 bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
+
+
+# Heleper functions
+def get_start_markup():
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 1
+    markup.add(InlineKeyboardButton("Start", callback_data="start"))
+    return markup
+
+
+def get_next_markup():
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 1
+    markup.add(
+        InlineKeyboardButton(
+            "Buy TEDx Ticket",
+            url="http://campus.covenantuniversity.edu.ng/tedxcovenant-university",
+        ),
+        InlineKeyboardButton("Call Support", url="https://t.me/favournelson"),
+    )
+    return markup
 
 
 @bot.message_handler(commands=["start"], chat_types=["private"])
