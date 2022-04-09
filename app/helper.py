358ace0805b33a1794e3d5b6ab4af27170e63c9d
Ebueku Uyiosa Peter <pebueku@gmail.com>
2022-04-09 21:15:22 +0100

    implemented referral feature

--- app/helper.py
+++ app/helper.py
@@ -1,13 +1,26 @@
+import os
+
+from dotenv import load_dotenv
+
 from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+load_dotenv()
 
 
 def get_start_markup():
+    """
+    Start markup
+    """
     markup = InlineKeyboardMarkup()
     markup.row_width = 1
     markup.add(InlineKeyboardButton("Start", callback_data="start"))
     return markup
 
+
 def get_next_markup():
+    """
+    Adds Markup for Bot functions
+    """
     markup = InlineKeyboardMarkup()
     markup.row_width = 1
     markup.add(
@@ -16,5 +29,6 @@ def get_next_markup():
             url="http://campus.covenantuniversity.edu.ng/tedxcovenant-university",
         ),
         InlineKeyboardButton("Call Support", url="https://t.me/favournelson"),
+        InlineKeyboardButton("Get personalised Link", callback_data="link"),
     )
     return markup
---
+++ app/mongo.py
@@ -0,0 +1,68 @@
+import os
+
+from dotenv import load_dotenv
+
+from pymongo import MongoClient
+
+load_dotenv()
+CONNECTION_STRING = os.environ["CONNECTION_STRING"]
+
+client = MongoClient(CONNECTION_STRING)
+
+db = client.get_default_database()
+leader_board = db.leaderboard
+
+
+def insert_new_participant(id: int, **kwargs):
+    """
+    Adds a participant to the leaderboard
+    """
+    if kwargs["username"]:
+        username = kwargs["username"]
+    else:
+        username = None
+
+    if kwargs["last_name"]:
+        last_name = kwargs["last_name"]
+    else:
+        last_name = None
+
+    first_name = kwargs["first_name"]
+    try:
+        leader_board.insert_one(
+            {
+                "_id": str(id),
+                "username": f"{username}",
+                "first_name": f"{first_name}",
+                "last_name": f"{last_name}",
+                "referrals": [],
+            }
+        )
+    except:
+        return
+
+
+def update_referral(referrer_string: str, user_id: int):
+    """
+    Updates the leader board
+    """
+    print("referral string", referrer_string)
+    referrer_id = referrer_string.split("_")[-2]
+    print("referral id", referrer_id)
+
+    # Checks if the user is trying to refer themself
+    if int(referrer_id) == user_id:
+        return
+
+    # Checks if the user has already been referred by someone else
+    if leader_board.count_documents({"referrals": f"{user_id}"}) > 0:
+        return
+
+    # Checks if the user already has a referral link
+    if leader_board.count_documents({"_id": f"{user_id}"}) > 0:
+        return
+
+    # Update leader board
+    leader_board.update_one(
+        {"_id": referrer_id}, {"$push": {"referrals": str(user_id)}}
+    )
